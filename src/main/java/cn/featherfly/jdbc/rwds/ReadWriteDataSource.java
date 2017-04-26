0b7aec1751211d8e5f142820330f4b17509d8b93
zhongji <zhongji@9imeihuo.com>
2017-04-26 18:34:41 +0800

    修复注释

--- src/main/java/cn/featherfly/jdbc/rwds/ReadWriteDataSource.java
+++ src/main/java/cn/featherfly/jdbc/rwds/ReadWriteDataSource.java
@@ -52,7 +52,7 @@ public class ReadWriteDataSource extends AbstractDataSource implements Initializ
      * 设置读库（name, DataSource）
      * 
      * @param readDataSourceMap
-     *            Map<String, DataSource>
+     *            Map&lt;String, DataSource&gt;
      */
     public void setReadDataSourceMap(Map<String, DataSource> readDataSourceMap) {
         this.readDataSourceMap = readDataSourceMap;
